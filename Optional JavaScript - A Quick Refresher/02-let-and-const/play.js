3aa914f317d75f1ef69c6204df8a3d9b4f71c005
MOR_DEV <mordev@morsoftware.com>
2021-06-04 11:12:51 +0700

    02-let-and-const

---
+++ Optional JavaScript - A Quick Refresher/02-let-and-const/play.js
@@ -0,0 +1,18 @@
+const name = "Richard";
+let age = 29;
+const hasHobbies = true;
+/** name = "Richard 2"; */
+age = 30;
+
+function summarizeUser(userName, userAge, userHasHobby) {
+  return (
+    "Name is " +
+    userName +
+    ", age is " +
+    userAge +
+    " and the user has hobbies: " +
+    userHasHobby
+  );
+}
+
+console.log(summarizeUser(name, age, hasHobbies));
